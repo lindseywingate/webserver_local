f75500763ab21b05afd3a6b9b6e49658e22f5b81
Lindsey Wingate <lindseymwingate@gmail.com>
2017-02-08 20:53:10 +0100

    practicing login. working!

--- CGI-Executables/checkLoginInfo.pl
+++ CGI-Executables/checkLoginInfo.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -wT
 
 use CGI qw(:standard);
+use DBI;
 
 print header(), start_html();
 
@@ -8,6 +9,23 @@ my $username = param ('txtUsername');
 my $password = param ('txtPassword');
 
 #here is where you would verify the username and password against a table in the database
+
+my $dbh = DBI->connect ("DBI:mysql:database=school;host=localhost", "root", "password") or die ("Couldn't make connection to database: $DBI:errstr");
+
+my $sth = $dbh->prepare ("SELECT * FROM tblusers where login = '$username' and password = '$password'") or die ("Cannot prepare statement.\n");
+
+$sth->execute() or die ("Cannot execute statement: ", $sth->errstr(), "\n");
+
+my @array;
+
+while (@array = $sth->fetchrow_array() ) {
+	printf "%10s %10s", @array;
+	print br();
+}
+
+$dbh->disconnect();
+$sth->finish();
+
 if ($username eq "admin" && $password eq "password")
 {
 	print("HIIIII");
