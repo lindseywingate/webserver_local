9647fc1012bbe4d08411a73ff3053467d2cea7d5
Lindsey Wingate <lindseymwingate@gmail.com>
2017-02-09 21:03:50 +0100

    set cookie WORKS!! finally. thanks to http://alvinalexander.com/perl/edu/articles/pl010012

--- CGI-Executables/makecookiepractice.pl
+++ CGI-Executables/makecookiepractice.pl
@@ -1,10 +1,7 @@
 #!/usr/bin/perl
 
 use CGI qw(:standard);
-use CGI::Session;
-use File::Spec;
-
-print header();
+$query = new CGI;
 
 my $username = param ('txtUsername');
 my $password = param ('txtPassword');
@@ -12,18 +9,16 @@ my $password = param ('txtPassword');
 #here is where you would verify the username and password against a table in the database
 if ($username eq "admin" && $password eq "password")
 {
-	#create cooke on the server with the user information
-	#1st argument - dsn info - leave blank (undef)
-	#2nd argument - session id, set to undef to create a new session 
-	#3rd argument - where should the cookie be store on the server
-	my $session = CGI::Session(undef, undef, {Directory=> '/tmp' });
-	$session->param('loggedin', 'yes');
-	$session->param('username', $username);
-	$session->close();
-	
-	my $cookie = cookie (-name=>'perl260',
-						-value => $session->id,
-						-expires => '+1m' );
-	print redirect (-cookie=>$cookie, -location=>'/cgi-bin/verifyLogin.pl'), start_html(), end_html();
-	
+	my $cookie = $query->cookie(-name=>"mycookie",
+		-value=>'bestcookie=whitechocochip',
+		-expires=>'+1m',
+		-path=>'/');
+
+	print $query->header(-cookie=>$cookie);
+	print $query->start_html('My cookie program');
+	print $query->h3('The cookie has been SET!');
 }
+else {
+	print $query->header();
+}
+	print $query->end_html;	
