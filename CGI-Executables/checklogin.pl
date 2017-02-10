48ce8158ba37b2a1a20023ce9eb0219375d53d0c
Lindsey Wingate <lindseymwingate@gmail.com>
2017-02-10 21:50:15 +0100

    got it working with cookies! changed some things in checklogininfo but still not sure if that works (was accident). user logs in, creates cookie, and goes to index.html.en

--- CGI-Executables/checklogin.pl
+++ CGI-Executables/checklogin.pl
@@ -2,12 +2,27 @@
 
 use CGI qw(:standard);
 $query = new CGI;
+use DBI;
 
 my $username = param ('txtUsername');
 my $password = param ('txtPassword');
 
+my $dbh = DBI->connect ("DBI:mysql:database=school;host=localhost", "root", "password");
+my $sth = $dbh->prepare ("SELECT count(*) from tblusers where login = '$username' and password = '$password'");
+
+$sth->execute();
+
+my @array;
+my $count=0;
+my $cookie;
+@array = $sth->fetchrow_array();
+
+foreach(@array) {
+	$count++;
+}	
+
 #here is where you would verify the username and password against a table in the database
-if ($username eq "admin" && $password eq "password")
+if ($count > 0)
 {
 	my $cookie = $query->cookie(-name=>"mycookie",
 		-value=>'bestcookie=whitechocochip',
